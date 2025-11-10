a0dbb4a2d05cfd99b4d9951238d8af08b9485a41
marjangdg <143639470+marjangdg@users.noreply.github.com>
2025-11-10 19:08:12 +0100

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from sunio import create_app
-
-@pytest.fixture()
-def client():
-    app = create_app()
-    app.config.update(TESTING=True)
-    with app.test_client() as client:
-        yield client
