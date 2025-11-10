afa9521bb9b0c1db5aded581eed39ac4596bac3f
marjangdg <143639470+marjangdg@users.noreply.github.com>
2025-11-10 18:43:25 +0100

    Add files via upload

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from sunio import create_app
+
+@pytest.fixture()
+def client():
+    app = create_app()
+    app.config.update(TESTING=True)
+    with app.test_client() as client:
+        yield client
